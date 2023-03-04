44d4b3c6e92a624843286a91edfae3b982948a4e
Oleg Bilas <olegbilas@gmail.com>
2023-03-04 21:08:59 +0200

    finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
+import Vimeo from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
 
+const serializatetData = localStorage.getItem('videoplayer-current-time');
+const seconds = serializatetData ? JSON.parse(serializatetData).seconds : 0;
+
+player
+  .setCurrentTime(seconds)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+const getTimeOfVideo = data => {
+  return localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+};
+
+player.on('timeupdate', throttle(getTimeOfVideo, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+
+//Відновлення значень із локального сховища
+const dataInputs = localStorage.getItem('feedback-form-state');
+if (dataInputs) {
+  const deserializatedValues = JSON.parse(dataInputs);
+  for (const key of Object.keys(deserializatedValues)) {
+    formRef.elements[key].value = deserializatedValues[key];
+  }
+}
+
+formRef.addEventListener('submit', onSubmitForm);
+formRef.addEventListener('input', throttle(onInputFormElements, 500));
+
+const formData = {};
+function onInputFormElements(event) {
+  if (event.target.name in formRef.elements) {
+    formData[event.target.name] = event.target.value;
+  }
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  event.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}

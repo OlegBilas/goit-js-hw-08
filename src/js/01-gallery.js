42f652bcdeaffe4ad829aa82524caa64f735bdf1
Oleg Bilas <olegbilas@gmail.com>
2023-03-04 15:26:26 +0200

    задача 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+galleryRef.innerHTML = createGalleryItems(galleryItems);
 
-console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                    />
+                </a>`;
+    })
+    .join('');
+}
+
+//console.log(galleryItems);

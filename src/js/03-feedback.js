7a853161b827d5257be1e52eeddf2978f93944b5
Oleg Bilas <olegbilas@gmail.com>
2023-03-04 21:15:38 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,11 @@
 import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
+const FEEDBACK_FORM_STATE = 'feedback-form-state';
+const formData = {};
 
 //Відновлення значень із локального сховища
-const dataInputs = localStorage.getItem('feedback-form-state');
+const dataInputs = localStorage.getItem(FEEDBACK_FORM_STATE);
 if (dataInputs) {
   const deserializatedValues = JSON.parse(dataInputs);
   for (const key of Object.keys(deserializatedValues)) {
@@ -14,16 +16,15 @@ if (dataInputs) {
 formRef.addEventListener('submit', onSubmitForm);
 formRef.addEventListener('input', throttle(onInputFormElements, 500));
 
-const formData = {};
 function onInputFormElements(event) {
   if (event.target.name in formRef.elements) {
     formData[event.target.name] = event.target.value;
   }
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(formData));
 }
 
 function onSubmitForm(event) {
   event.preventDefault();
   event.target.reset();
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(FEEDBACK_FORM_STATE);
 }
